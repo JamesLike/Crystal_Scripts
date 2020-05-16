2e93029d2ebb6b574664b2186e8971c5f0a5bbf1
JamesLike <bax.j@hotmail.co.uk>
2020-05-16 22:02:05 +0100

    updated loc and fixed param files

--- Cryst_FEL/crystfel_stats_merge.sh
+++ Cryst_FEL/crystfel_stats_merge.sh
@@ -36,6 +36,23 @@ if [ "X$NUM_TOTAL_REFL" != "X$SUM_MEASURED" ] ; then
 else
     OVERALL_COMPLETENESS=$(printf "%.2f" $(echo 100*$SUM_MEASURED/$SUM_POSSIBLE | bc -l) )
 fi
+
+RES_LIMS=$(grep  "^Accepted" $FILENAME_LOG | head -1 | awk '{print $8"-"$10}')
+RES_LIMS=${RES_LIMS#?}
+H_RES_LIMS=$(tail $FILENAME_RSPLIT -n 1 | awk '{printf "%0.3f-%0.3f", 10/$5, 10/$6}')
+
+sigtonoise=$(grep "^Overall <snr> " $FILENAME_LOG | awk '{printf "%0.3f", $4}')
+
+
+rsplit=$(grep "^Overall Rsplit* " $FILENAME_LOG | awk '{printf "%0.2f", $4}')
+
+ccstar=$(grep "^Overall CC* " $FILENAME_LOG | awk '{printf "%0.2f", $4}')
+
+cc=$(grep "^Overall CC " $FILENAME_LOG | awk '{printf "%0.2f", $4}')
+
+wilsonb=$( grep "B =" wilson.dat | awk '{print $3}')
+
+
 echo "-Cutting"
 # cut -c 84-90       $FILENAME_SNR    >$TEMP_RES
 cut -c 38-44       $FILENAME_CCstar     >$TEMP_RES
@@ -43,22 +60,42 @@ cut -c 29-36,39-59 $FILENAME_SNR    >$TEMP_SNR
 cut -c 14-22       $FILENAME_RSPLIT >$TEMP_RSPLIT
 cut -c 13-22       $FILENAME_CCstar     >$TEMP_CCstar
 cut -c 13-22       $FILENAME_CC     >$TEMP_CC
-grep "^Number of crystals:" -A 20 $FILENAME_LOG | grep -B 20 " total  (" | tail -n +2  >$FILENAME_OUT
-echo >>$FILENAME_OUT
 
-grep "first hit: "  $FILENAME_LOG   >>$FILENAME_OUT
-grep "last hit: "   $FILENAME_LOG   >>$FILENAME_OUT
-echo >>$FILENAME_OUT
-
+#########################
+# Now maps the combined output file
+#########################
 echo "overall Compl. = $OVERALL_COMPLETENESS   (measured $SUM_MEASURED of $SUM_POSSIBLE reflections)"             >>$FILENAME_OUT
 echo "Total number of Crystals: $Number_images " >>$FILENAME_OUT
 echo "overall Red.   = $OVERALL_REDUNDANCY   ($NUM_TOTAL_MEAS measurements of $NUM_TOTAL_REFL reflections)"       >>$FILENAME_OUT
 grep "^overall "                  $FILENAME_LOG | sed -e 's| <snr> = | <snr>  = |g'                               >>$FILENAME_OUT
 grep "^Overall "                  $FILENAME_LOG | sed -e 's|Overall |overall |g' | sed -e 's| CC\* = | CC*    = |g' >>$FILENAME_OUT
-#grep "Overall CC"  $FILENAME_LOG >> $FILENAME_OUT
+grep "Overall CC"  $FILENAME_LOG >> $FILENAME_OUT
 grep "B =" wilson.dat >> $FILENAME_OUT
 echo >>$FILENAME_OUT
 paste -d " " $TEMP_RES $TEMP_SNR $TEMP_RSPLIT $TEMP_CC $TEMP_CCstar                               >>$FILENAME_OUT
+
+TABLE1="Tabl.dat"
+hmeasured=$(tail $FILENAME_OUT -n 1 | awk '{printf "%0.0f", $3}')
+hcomple=$(tail $FILENAME_OUT -n 1 | awk '{printf "%0.2f", $2}')
+hsnr=$(tail $FILENAME_OUT -n 1 | awk '{printf "%0.2f", $5}')
+hrsplit=$(tail $FILENAME_OUT -n 1 | awk '{printf "%0.2f", $6}')
+hccstar=$(tail $FILENAME_OUT -n 1 | awk '{printf "%0.2f", $8}')
+hcc=$(tail $FILENAME_OUT -n 1 | awk '{printf "%0.2f", $7}')
+
+
+
+echo -e "Resolution Limits \AA:         \t ${RES_LIMS} (${H_RES_LIMS})"           >$TABLE1
+echo -e "Merged Crystals:               \t $Number_images "                       >>$TABLE1
+echo -e "No. Unique reflection Indicies:\t $SUM_POSSIBLE "                        >>$TABLE1
+echo -e "No. Merged   Reflections:      \t $NUM_TOTAL_MEAS (${hmeasured})"        >>$TABLE1
+echo -e "Completeness (%):              \t $OVERALL_COMPLETENESS (${hcomple})"    >>$TABLE1
+echo -e "Signal to noise:               \t $sigtonoise (${hsnr})"                 >>$TABLE1
+echo -e "Wilson b factor:               \t $wilsonb"                              >>$TABLE1
+echo -e "R\$_{Split}\$ (%):             \t $rsplit (${hrsplit})"                  >>$TABLE1
+echo -e "CC*                            \t $ccstar (${hccstar})"                  >>$TABLE1
+echo -e "CC\$_{1/2}\$                   \t $cc (${hcc})"                          >>$TABLE1
+
+
 cp $FILENAME_OUT "Stats_summary.log"
 echo "-Done"
 rm $TEMP_RSPLIT $TEMP_CC $TEMP_RES $TEMP_SNR
